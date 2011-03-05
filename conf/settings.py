5dcf12595f0b2654b0043e9175fe9c9231388b10
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-03-05 03:13:50 -0400

    Updated configuration option names

--- conf/settings.py
+++ conf/settings.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 
 
-GRIDFS_HOST = getattr(settings, 'STORAGE_GRIDFS_HOST', 'localhost')
-GRIDFS_PORT = getattr(settings, 'STORAGE_GRIDFS_PORT', 27017)
-DATABASE_NAME = getattr(settings, 'STORAGE_GRIDFS_DATABASE_NAME', u'gridfs_storage')
+GRIDFS_HOST = getattr(settings, 'LEPRECHAUN_GRIDFS_HOST', 'localhost') # or list ['host a', 'host b']
+GRIDFS_PORT = getattr(settings, 'LEPRECHAUN_GRIDFS_PORT', 27017)
+GRIDFS_DATABASE_NAME = getattr(settings, 'LEPRECHAUN_GRIDFS_DATABASE_NAME', u'gridfs_storage')
+
+
